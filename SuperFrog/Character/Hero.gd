d5176ba45d07cc141973cdf9e87b1269b8e22e65
gljubojevic <gljubojevic@users.noreply.github.com>
2024-03-29 22:04:04 +0100

    Add Hero jumping

--- SuperFrog/Character/Hero.gd
+++ SuperFrog/Character/Hero.gd
@@ -1,21 +1,24 @@
 extends CharacterBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 150
-
-#var velocity = Vector2()
+@export var GRAVITY = 200.0
+@export var WALK_SPEED = 150
+@export var JUMP_SPEED = 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+	#set_up_direction(Vector2(0, -1))
+	set_floor_stop_on_slope_enabled(false)
+	set_floor_constant_speed_enabled(true)
+	#set_floor_snap_length(1)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
-
+	# handle left/right
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
 	elif Input.is_action_pressed("ui_right"):
@@ -23,10 +26,12 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 
+	# handle falling down
+	velocity.y += delta * GRAVITY
+	# handle jumping
+	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		velocity.y += -JUMP_SPEED
+	
 	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
 	set_velocity(velocity)
-	set_up_direction(Vector2(0, -1))
 	move_and_slide()
